b4d639b2bf96f1409c026c9cdf969db5e29497f4
Marcitox <benitez4721@gmail.com>
2020-06-16 15:24:19 -0400

    logica para el login junto con validaciones de correo

--- queries/queries.js
+++ queries/queries.js
@@ -2,7 +2,7 @@
 const db = require('../server/db')
 
 function crearTablas(){
-    let sql = 'CREATE TABLE IF NOT EXISTS usuarios (id INT PRIMARY KEY,nombre VARCHAR(255), password VARCHAR(255))'
+    let sql = 'CREATE TABLE IF NOT EXISTS usuarios (id INT PRIMARY KEY,email VARCHAR(255) UNIQUE, password VARCHAR(255))'
     db.query(sql, (err, result) => {
         if( err ) {
             console.log(err);
@@ -15,27 +15,67 @@ function crearTablas(){
 }
 
 function crearUsuario(body, res){
-    let sql = `INSERT INTO usuarios (nombre, password) VALUES (${body.nombre},AES_ENCRYPT(${body.password},'secret'))`
-    db.query(sql, (err, result) => {
-        if( err ) {
-            console.log("aqui toy");
-            
+
+    // Verificamos que el correo no ha sido ingresado en la base de datos
+    let sqlEmail = `SELECT email FROM usuarios WHERE email = '${body.email}'`
+    db.query(sqlEmail, (err, result,fields) => {
+        if(err) {
             console.log(err);
-            return  res.status(400).json({
+            return res.status(400).json({
                 ok: false,
-                err
-            })  
+                mensaje: "Fallo en querie buscar email"
+            })
+            
         }
-        res.json({
-            ok: true,
-            mensaje: "Registro insertado correctamente"
+
+        if(result.length > 0){
+            return res.status(400).json({
+                ok: false,
+                mensaje: "Este correo ya ha sido utilizado"
+            })
+        }
+        let sql = `INSERT INTO usuarios (email, password) VALUES ('${body.email}',AES_ENCRYPT(${body.password},'secret'))`
+        db.query(sql, (err, result) => {
+            if( err ) {
+                console.log("aqui toy");
+                return  res.status(400).json({
+                    ok: false,
+                    err
+                })  
+            }
+            res.json({
+                ok: true,
+                mensaje: "Registro insertado correctamente"
+            })
+            
         })
         
+        
+        
+    })
+ 
+   
+}
+
+function verificarCorreo(correo) {
+    let sqlEmail = `SELECT email FROM usuarios WHERE email = '${correo}'`
+    db.query(sqlEmail, (err, result,fields) => {
+        if(err) {
+            
+            return false            
+        }
+
+        if(result.length > 0){
+            return true
+        }
+        console.log("aqui");
+        return false
     })
 }
 
 
 module.exports = {
     crearTablas,
-    crearUsuario
+    crearUsuario,
+    verificarCorreo,
 }
--- routes/login.js
+++ routes/login.js
@@ -8,8 +8,8 @@ const app = express()
 
 app.post('/registro',[
 
-    check('password').isLength({min:5}).withMessage('Debe ser de al menos 5 caracteres')
-    
+    check('password').isLength({min:5}).withMessage('Debe ser de al menos 5 caracteres'),
+    check('email').matches(/\w+@[a-zA-Z]+\.[a-zA-Z]/).withMessage("email invalido")
 
 ] ,(req,res) => {
 
@@ -24,6 +24,53 @@ app.post('/registro',[
     queries.crearUsuario(body,res)
     
     
+})
+
+app.post('/login', (req, res) => {
+    let sqlEmail = `SELECT email FROM usuarios WHERE email = '${req.body.email}'`
+    db.query(sqlEmail, (err, result,fields) => {
+        if(err) {
+            
+            return res.status(400).json({
+                ok: false,
+                err
+            })           
+        }
+
+        if(result.length < 1){
+            return res.json({
+                ok: false,
+                mensaje: "El (correo) o la contrasena no es valido"
+            })
+        }
+
+        let sqlPassword = `SELECT password FROM usuarios WHERE password = AES_ENCRYPT(${req.body.password},'secret')`
+        db.query(sqlPassword, (err,result,fields) => {
+            if(err){
+                
+                return res.status(400).json({
+                    ok: true,
+                    err
+                })
+                
+            }
+            if(result < 1){
+                return res.json({
+                    ok: false,
+                    mensaje: "El correo o la (contrasena) no es valido"
+                })
+            }
+
+            return res.json({
+                ok: true,
+                mensaje: `Usuario ${req.body.email} loggeado`
+            })
+            
+
+        })
+        
+        
+    })
 })
 
 module.exports = app
